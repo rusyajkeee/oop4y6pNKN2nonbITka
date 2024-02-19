1de9d6eef5665c601b12d88c1e7dd0f533b1cead
rusyajkeee <230432@astanait.edu.kz>
2024-02-19 16:21:36 +0600

    Changes by Waitan

--- src/main/java/com/example/oop4y6riki/Application.java
+++ src/main/java/com/example/oop4y6riki/Application.java
@@ -3,6 +3,9 @@ package com.example.oop4y6riki;
 import ch.qos.logback.core.encoder.JsonEscapeUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class Application {

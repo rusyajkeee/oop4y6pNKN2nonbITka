f5d792d461a5a27b531b4486389e5b8cd57bf4dc
rusyajkeee <230432@astanait.edu.kz>
2024-02-19 15:53:43 +0600

    юзер добавлен

--- src/main/java/com/example/oop4y6riki/Application.java
+++ src/main/java/com/example/oop4y6riki/Application.java
@@ -1,5 +1,6 @@
 package com.example.oop4y6riki;
 
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/example/oop4y6riki/User.java
@@ -0,0 +1,7 @@
+package com.example.oop4y6riki;
+
+public class User {
+    public void sayHi(){
+        System.out.println("Priveet");
+    }
+}
